b26f63d56bcdc4276af7ee3c4096c90fde86f1ec
taylor-oconnell <t89oconnell@gmail.com>
2018-01-11 20:32:25 -0500

    Got rid of hard-coded database name; changed to command line argument.

--- scripts/magicblast_sra.sh
+++ scripts/magicblast_sra.sh
@@ -2,5 +2,5 @@
 #!/bin/bash
 while read p; do
 	echo "Processing SRA run $p."
-	magicblast -sra $p -num_threads 6 -outfmt sam -out sam_output/$p.sam -no_query_id_trim  -splice F -no_unaligned  -db /data/RefSeq/viral.genomic.fna;
+	magicblast -sra $p -num_threads 6 -outfmt sam -out sam_output/$p.sam -no_query_id_trim  -splice F -no_unaligned  -db $2;
 done <$1
