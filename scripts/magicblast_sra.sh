642058f74bb62e8138014d14f7a2043c409f84c3
Robert Edwards <raedwards@gmail.com>
2018-01-11 17:37:14 -0800

    adding an error message

--- scripts/magicblast_sra.sh
+++ scripts/magicblast_sra.sh
@@ -1,5 +1,13 @@
-
 #!/bin/bash
+
+
+if [ ! -n "$1" ] || [ ! -n "$2" ]; then
+	echo "Usage: `basename $0` <list of SRA IDs> <magic blast database>"
+	exit $E_BADARGS
+fi
+
+
+
 while read p; do
 	echo "Processing SRA run $p."
 	magicblast -sra $p -num_threads 6 -outfmt sam -out sam_output/$p.sam -no_query_id_trim  -splice F -no_unaligned  -db /data/RefSeq/viral.genomic.fna;
